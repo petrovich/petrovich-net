e6738f960321978a1a08728d043bdcb1162d09fe
Vitali Fokin <vitali.fokin@gmail.com>
2013-10-12 14:12:56 +0300

    add rules.yml validation tests

---
+++ NPetrovich.Tests/Fixtures/RulesFixture.cs
@@ -0,0 +1,17 @@
+﻿using NPetrovich.Rules.Loader.Yaml;
+using NUnit.Framework;
+
+namespace NPetrovich.Tests.Fixtures
+{
+    [TestFixture]
+    public class RulesFixture
+    {
+        [Test]
+        public void Rules_yml_should_be_valid_yaml_file()
+        {
+            YamlRulesLoader loader = new YamlRulesLoader("rules.yml");
+
+            Assert.DoesNotThrow(() => loader.Load());
+        }
+    }
+}
