21e8721565a823eb0d0d5656950dd95bc97f430c
Vitali Fokin <vitali.fokin@gmail.com>
2014-01-23 11:37:07 +0300

    extract test data factory to separate file

---
+++ src/NPetrovich.Tests/TestDataProviders/InflectionTestCaseDataFactory.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Linq;
+
+namespace NPetrovich.Tests.TestDataProviders
+{
+    public class InflectionTestCaseDataFactory
+    {
+        public static IEnumerable LastNamesInflectionData
+        {
+            get
+            {
+                using (var reader = new StreamReader(Path.Combine("Data", "LastNames.csv")))
+                {
+                    foreach (var p in ReadCaseData(reader)) yield return p;
+                }
+            }
+        }
+
+        public static IEnumerable FirstNamesInflectionData
+        {
+            get
+            {
+                using (var reader = new StreamReader(Path.Combine("Data", "FirstNames.csv")))
+                {
+                    foreach (var p in ReadCaseData(reader)) yield return p;
+                }
+            }
+        }
+
+        public static IEnumerable MiddleNamesInflectionData
+        {
+            get
+            {
+                using (var reader = new StreamReader(Path.Combine("Data", "MiddleNames.csv")))
+                {
+                    foreach (var p in ReadCaseData(reader)) yield return p;
+                }
+            }
+        }
+
+        private static IEnumerable ReadCaseData(StreamReader reader)
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                    continue;
+
+                var chunks = line.Split(',').Select(s => s.Trim()).ToList();
+
+                var gender = (Gender)Enum.Parse(typeof(Gender), chunks[1]);
+                var @case = (Case)Enum.Parse(typeof(Case), chunks[2]);
+
+                yield return new object[] { chunks[0], gender, @case, chunks[3] };
+            }
+        }
+
+    }
+}
